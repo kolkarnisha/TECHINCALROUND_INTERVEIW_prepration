9f9c51fe6c04a4b394260e97c2ef6841aa0502ed
Kolkar Nisha <neeshakolkar@gmail.com>
2026-07-22 19:01:02 +0530

    Refactor list operations and add new functionalities
    Refactored code for clarity and added various operations on lists, including finding the maximum, reversing arrays, and checking for even numbers.

--- miniproject3.py
+++ miniproject3.py
@@ -5,3 +5,48 @@ def largestnum(list):
             if num>list:
                   largest=num
                   print(largest)
+n=[1,"nisha",22,6.8,"taj"]
+for i in n:
+    # for j in n:
+      print(i) # traverse by value
+time and space complexity
+a loop after loop is called consecutive,sequential loop
+numbers=[10,27,38,46,59]
+for index,j in enumerate(numbers):
+    print(index,j)
+reversearray=numbers[::-1]
+print(reversearray)
+list=[10,20,30,40,50]
+count=0
+while count<len(list):
+    print(list[count])
+    count+=1
+sum=sum(numbers)
+print(sum)  #time complexity 
+sum=0
+for i in numbers:
+    i+sum
+    print(sum)
+n=0
+for i in numbers:
+    # n=0
+    if i%2==0:
+        print(i)
+        n+=1
+        print(n)
+    else:
+        print("odd number")
+        print(i)
+numbers=[4,8,12,16,20]
+if 12 in numbers:
+    print("true")
+numbers=[4,8,12,16,20]
+for i in numbers:
+    if i==16:
+        print(numbers.index(i))
+list=[100,200,300,400]
+for i in list:
+    print(i)
+revrsearray=list[::-1]
+for i in revrsearray:
+    print(i)
