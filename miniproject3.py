16274f0cc1203b91de53546afb2ee65b2bbd07c4
Kolkar Nisha <neeshakolkar@gmail.com>
2026-07-22 16:09:45 +0530

    Implement largestnum function to find largest number
    Fix the largestnum function to properly compare numbers.

---
+++ miniproject3.py
@@ -0,0 +1,7 @@
+list=[2,21,2210,221027]
+print(max(list))
+def largestnum(list):
+      for num in list:          
+            if num>list:
+                  largest=num
+                  print(largest)
