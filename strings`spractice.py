e9ec451537a45f77bf0c0d1807b71618ecb8b0eb
Kolkar Nisha <neeshakolkar@gmail.com>
2026-07-23 17:12:37 +0530

    Implement various string manipulations in spractice.py
    This script demonstrates various string operations including concatenation, slicing, case conversion, and joining. It also includes examples of counting vowels and replacing spaces with hyphens.

---
+++ strings`spractice.py
@@ -0,0 +1,61 @@
+string="zareentaj"
+print(string[0])
+string2="algonex"
+print(string+string2)
+for i in string,string2:
+    print(i)
+#print(string[4]==("j"))
+string="j"+string[4]
+print(string)
+new="algonexitsolutions"
+print(new[0:3])
+print(new[2:])
+print(new[::-1])
+print(new*3)
+for i in range(len(new)):
+    if new[i]=="l":
+        print(len(new))
+# for i in range("algonexitsolutions"):
+#     if new[i]=="l":
+#         print(i.index(i))
+str1="spark"
+str2="trainee"
+print(str1+" "+str2)
+str="algonex it solutions"
+print(str.upper())
+print(str[0].upper())
+print(str)
+print(str.capitalize())
+print(str.title())
+print(str.lower())
+print(str.replace("it","tec"))
+print(str.split())
+new="$"
+str.join(new)
+print(str)
+print("$".join(str))
+print("$".join(str.split()))
+# str="ganeshpasala"
+# def removedup():
+#     for i in str:
+#         print(i)
+# removedup(str)
+#find length of a string...3
+str='nishazareentaj'
+for i in range(len(str)):
+    if str[i]=="j":
+        print(len(str))
+#count of vowel...4
+count=""
+for i in str:
+    if str=="a"and "e" and "i" and "o" and "u":
+        count+=1
+        print(count)
+#replace spaces with hyphens
+str1="nisha   zareen   taj"
+print(str.replace("  ","_"))
+
+
+
+
+  
